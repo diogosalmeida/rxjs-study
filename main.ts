353aceb9e0f9c14fdafea5e16d63953975581c0c
diogosalmeida <diogomean@gmail.com>
2017-11-18 13:59:47 -0200

    Primeiro commit estudo de programação reativa com rxjs

---
+++ main.ts
@@ -0,0 +1,26 @@
+import { Observable, Observer } from 'rxjs';
+
+let numbers = [1,5,10];
+let source = Observable.create(observer => {
+    let index = 0;
+    let produceValue = () => {
+        observer.next(numbers[index++]);
+
+        if(index < numbers.length){
+            setTimeout(produceValue,2000);
+        }
+        else {
+            observer.complete();
+        }
+    }
+    produceValue()
+}).map(n => n * 2).filter(n => n > 4);
+
+
+
+
+source.subscribe(
+    value => console.log(`value: ${value}`),
+    error => console.log(`error ${error}`),
+    () => console.log('complete')
+);
